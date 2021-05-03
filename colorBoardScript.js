68987a6c8a7ab1721225600c2b23a1a54272711b
Chayanne Sandoval-Williams <stuffedpeppers12@gmail.com>
2021-05-03 00:36:56 -0500

    maybe last commit of the night
    we will see
    added the files for mood and color board
    fixed carousel and added color images for testing

---
+++ colorBoardScript.js
@@ -0,0 +1,26 @@
+/*
+    Chayanne Sandoval-Williams
+    Scripts to change pictures and links on the page in order to save layout html pages
+
+    TODO: add in script details
+*/
+
+function onClickPastels()
+{
+    setTimeout(function(){window.location='color_board.html'}, 1000)
+}
+
+function onClickNeons()
+{
+    setTimeout(function(){window.location='color_board.html'}, 1000)
+}
+
+function onClickNeutrals()
+{
+    setTimeout(function(){window.location='color_board.html'}, 1000)
+}
+
+function onClickBasics()
+{
+    setTimeout(function(){window.location='color_board.html'}, 1000)
+}
