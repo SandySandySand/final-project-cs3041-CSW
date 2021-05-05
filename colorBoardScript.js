6297b558e8d9bad9a2d4d55dbd039bb06cc5d932
Chayanne Sandoval-Williams <stuffedpeppers12@gmail.com>
2021-05-05 00:55:52 -0500

    may 4th changes
    i still have so much to do, but I am closer that I was yesterday.
    Edited color boards so they are all working
    added in framework for noun and prompt generation
    created textual inspo landing page

--- colorBoardScript.js
+++ colorBoardScript.js
@@ -17,7 +17,19 @@ function onClickNeons()
 
 function onClickNeutrals()
 {
-    //document.getElementById("img1").src = 
+    document.getElementById("colorHeader").innerHTML = "Neutral Color Board"
+    document.getElementById("img1").src = "images\n_brown.png"
+    //document.getElementById("img1").href = #
+    document.getElementById("img2").src = "images\n_green.png"
+    //document.getElementById("img2").href = #
+    document.getElementById("img3").src = "images\n_grey.png"
+    //document.getElementById("img3").href = #
+    document.getElementById("img4").src = "images\n_ivory.png"
+    //document.getElementById("img4").href = #
+    document.getElementById("img5").src = "images\n_navy.png"
+    //document.getElementById("img5").href = #
+    document.getElementById("img6").src = "images\n_tan.png"
+    //document.getElementById("img6").href = #
     setTimeout(function(){window.location='color_board.html'}, 1000)
 }
 
